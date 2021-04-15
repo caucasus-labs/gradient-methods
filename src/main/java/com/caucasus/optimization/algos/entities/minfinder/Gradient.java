4a0e21603f74fc857c3acbf2495cace244be9713
IamMOTY <mysmplmail@gmail.com>
2021-04-15 23:32:55 +0300

    fart code done

--- src/main/java/com/caucasus/optimization/algos/entities/minfinder/Gradient.java
+++ src/main/java/com/caucasus/optimization/algos/entities/minfinder/Gradient.java
@@ -1,26 +1,51 @@
 package com.caucasus.optimization.algos.entities.minfinder;
 
-import com.caucasus.optimization.algos.entities.util.QuadraticFunction;
-import com.caucasus.optimization.algos.entities.util.Vector;
+import com.caucasus.optimization.algos.entities.util.*;
 import com.caucasus.optimization.algos.interfaces.GradientMethod;
-import com.caucasus.optimization.algos.entities.util.GradientSolution;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 public class Gradient implements GradientMethod {
     private final QuadraticFunction function;
     private final Double eps;
+    private final Domain domain;
 
-    public Gradient(QuadraticFunction function, Double eps) {
+    public Gradient(QuadraticFunction function, Double eps, Domain domain) {
         this.function = function;
         this.eps = eps;
+        this.domain = domain;
     }
 
     @Override
     public GradientSolution getSolution() {
-        Vector x = new Vector(new ArrayList<>(Collections.nCopies(function.size(), 1.)));
-        Vector antigradient = function.gradient(x).mul(-1.);
-        return null;
+        List<Vector> points = new ArrayList<>();
+        List<Double> values = new ArrayList<>();
+        int iterations = 0;
+        points.add(domain.middle());
+        values.add(function.apply(points.get(0)));
+        Vector gradient = function.getGradient(points.get(0));
+        double learningRate = function.getLearningRate(gradient, gradient.mul(-1));
+        do {
+            if (iterations != 0) {
+                gradient = function.getGradient(points.get(0));
+            }
+            while (true){
+                Vector newPoint = function.shiftVector(points.get(iterations), gradient, learningRate);
+                double newValue = function.apply(newPoint);
+                if (newValue < values.get(iterations)) {
+                    points.add(newPoint);
+                    values.add(newValue);
+                    iterations++;
+                    break;
+                } else {
+                    learningRate *= 0.5;
+                }
+            }
+        } while (Math.abs(values.get(iterations) - values.get(iterations - 1)) > eps ||
+        points.get(iterations).dist(points.get(iterations - 1)) > eps ||
+        gradient.length() > eps);
+        return new GradientSolution(points, values);
     }
+
 }
