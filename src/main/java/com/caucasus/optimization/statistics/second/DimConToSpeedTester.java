752be8451d3beb7dba5739f72f61c97c9a055dd7
Nikita Kozhukharov <n.korzhikov@gmail.com>
2021-05-11 10:08:02 +0300

    new tester

--- src/main/java/com/caucasus/optimization/statistics/second/DimConToSpeedTester.java
+++ src/main/java/com/caucasus/optimization/statistics/second/DimConToSpeedTester.java
@@ -15,6 +15,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 
@@ -24,8 +28,8 @@ import java.util.stream.Collectors;
  * @author nkorzh
  */
 public class DimConToSpeedTester {
-    private final static int MAX_DIMENSION = 10000;
-    private final static int TESTS_AMOUNT = 5;
+    private final static int MAX_DIMENSION = 1000;
+    private final static int TESTS_PER_POINT = 5;
     private final static int CONDITIONAL_STEP = 100;
     private final static int MAX_CONDITIONAL_NUMBER = 2200;
 
@@ -54,53 +58,35 @@ public class DimConToSpeedTester {
 
     public static void main(String[] args) {
         List<Class<?>> methodTypes = List.of(
-                Conjugate.class,
+                Conjugate.class
+//                ,
 //                SteepestDescent.class
 //                ,
-                Gradient.class
+//                Gradient.class
         );
         // for each method
         List<Thread> threads = new ArrayList<>();
         for (Class<?> methodType : methodTypes) {
             Thread thread = new Thread(() -> {
-                List<Thread> treads = new ArrayList<>();
-                for (int dimension = 10; dimension <= MAX_DIMENSION; dimension *= 10) {
-                    int dim = dimension;
-                    Thread tread = new Thread(() -> {
-                        List<ChartPoint> iterationsToK = new ArrayList<>();
-                        for (int conditionalNumber = 2; conditionalNumber <= MAX_CONDITIONAL_NUMBER;
-                             conditionalNumber += CONDITIONAL_STEP) {
-                            int iterationsCount = 0;
-                            for (int test = 0; test < TESTS_AMOUNT; test++) {
-                                final QuadraticFunction function = generateFunction(dim, conditionalNumber);
-                                final Domain domain = new Domain(
-                                        new Vector(new ArrayList<>(Collections.nCopies(dim, -100.))),
-                                        new Vector(new ArrayList<>(Collections.nCopies(dim, 100.)))
-                                );
-                                final double eps = 1e-6;
-                                try {
-                                    GradientMethod method = (GradientMethod) methodType
-                                            .getDeclaredConstructor(QuadraticFunction.class, Double.class, Domain.class)
-                                            .newInstance(function, eps, domain);
-                                    iterationsCount += method.getIterations(); //getSolution().getIterations();
-                                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                                        NoSuchMethodException e) {
-                                    System.err.println("Couldn't create instance of " +
-                                            methodType.getSimpleName() + ": " + e.getMessage());
-                                }
-                            }
-                            iterationsToK.add(new ChartPoint(conditionalNumber, (double) iterationsCount / TESTS_AMOUNT));
-                        }
-                        writePoints(methodType.getSimpleName().toLowerCase(), String.valueOf(dim), iterationsToK);
+                ExecutorService workers = Executors.newFixedThreadPool(10);
+                ExecutorService dimworkers = Executors.newFixedThreadPool(5);
+                final int startDim = 1000;
+                CountDownLatch dimensions = new CountDownLatch((int) (Math.log10(MAX_DIMENSION / startDim) + 1));
+                for (int dimension = startDim; dimension <= MAX_DIMENSION; dimension *= 10) {
+                    final int dim = dimension;
+                    dimworkers.submit(() -> {
+                        scheduleChart(workers, dim, methodType);
+                        System.err.println("Done " + methodType.getSimpleName() + ", dim: " + dim);
+                        dimensions.countDown();
                     });
-                    treads.add(tread);
-                    tread.start();
                 }
-                for (var tha : treads) {
-                    try {
-                        tha.join();
-                    } catch (InterruptedException ignored) {
-                    }
+                try {
+                    dimensions.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    workers.shutdown();
+                    dimworkers.shutdown();
                 }
             });
             threads.add(thread);
@@ -112,9 +98,70 @@ public class DimConToSpeedTester {
             } catch (InterruptedException ignored) {
             }
         }
-
-
     }
+
+    private static void scheduleChart(final ExecutorService workers, final int dim, final Class<?> methodType) {
+        final int iterations = MAX_CONDITIONAL_NUMBER / CONDITIONAL_STEP;
+        List<ChartPoint> iterationsToK = new ArrayList<>(Collections.nCopies(iterations, null));
+        CountDownLatch chartXValues = new CountDownLatch(iterations);
+
+        for (int i = 0; i < iterations; i++) {
+            final int iter = i;
+            final int finalConditionalNumber = 2 + CONDITIONAL_STEP * i;
+            workers.submit(() -> {
+                ExecutorService ww = Executors.newFixedThreadPool(TESTS_PER_POINT);
+                final AtomicInteger iterationsCount = new AtomicInteger(0);
+                final CountDownLatch testsPerMu = new CountDownLatch(TESTS_PER_POINT);
+                for (int test = 0; test < TESTS_PER_POINT; test++) {
+                    int testNum = test;
+                    ww.submit(() -> {
+                        iterationsCount.getAndAdd(getIterations(dim, finalConditionalNumber, methodType)); //getSolution().getIterations();
+                        testsPerMu.countDown();
+                        System.out.println(methodType.getSimpleName() + ": " + iter + " -- " + testNum);
+                    });
+                }
+                try {
+                    testsPerMu.await();
+                    iterationsToK.set(iter, new ChartPoint(finalConditionalNumber, (double) iterationsCount.get() / TESTS_PER_POINT));
+                    System.out.println("Done " + iter + " iter");
+                } catch (InterruptedException e) {
+                    System.err.println("Interrupted at " + iter);
+                    e.printStackTrace();
+                }
+                chartXValues.countDown();
+                ww.shutdown();
+            });
+        }
+        try {
+            chartXValues.await();
+            writePoints(methodType.getSimpleName().toLowerCase(), String.valueOf(dim), iterationsToK);
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted while waiting for chart counting finishes");
+        }
+    }
+
+    private static int getIterations(final int dim, final int condNumber, final Class<?> methodType ) {
+        final QuadraticFunction function = generateFunction(dim, condNumber);
+        final Domain domain = new Domain(
+                new Vector(new ArrayList<>(Collections.nCopies(dim, -100.))),
+                new Vector(new ArrayList<>(Collections.nCopies(dim, 100.)))
+        );
+        final double eps = 1e-5;
+        try {
+            GradientMethod method = (GradientMethod) methodType
+                    .getDeclaredConstructor(QuadraticFunction.class, Double.class, Domain.class)
+                    .newInstance(function, eps, domain);
+            return method.getIterations(); //getSolution().getIterations();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                NoSuchMethodException e) {
+            System.err.println("Couldn't create instance of " +
+                    methodType.getSimpleName() + ": " + e.getMessage());
+        }
+        System.err.println("Error counting iterations!");
+        return 0;
+    }
+
+
 
     private static void writePoints(final String methodName, final String chartName, final List<ChartPoint> points) {
         final String fileName = String.join(File.separator,
